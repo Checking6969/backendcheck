92ae064b8a9c3b5b0f9bb8367f4c62cc04c3f81c
nevernever69 <nevernevernever69@protonmail.com>
2024-05-09 10:17:41 +0530

    port1

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const UserModel = require('./models/Usermodel');
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.send("got Cd and here is the dummy get request");
 });
-app.listen(443, () => {
+app.listen(3000, () => {
     console.log('server is running and here we gos');
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import mongoose, { mongo } from 'mongoose';
-const UserModel = require('./models/Usermodel')
 const app = express();
 
 app.use(cors());
@@ -10,6 +9,6 @@ app.use(express.json());
 app.get('/', (req:Request, res:Response) => {
 		res.send("got Cd and here is the dummy get request")
 });
-app.listen(443, () => {
+app.listen(3000, () => {
 	console.log('server is running and here we gos')
 })
